4c67747829ed1c99e970319eb07b46f60f77f8de
fkaraagac11 <fehmi11.qa@gmail.com>
2020-07-30 22:24:15 -0400

    added

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//JSX
+
+ReactDOM.render(
+    <div>
+        <h1>Hello World</h1>
+    </div>,
+    document.getElementById("root")
+);
+
+var Element = document.createElement("p");
+Element.innerHTML = "This is a pataghraf";
