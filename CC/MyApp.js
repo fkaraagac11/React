3503e85e6684ca32c69a0025e0318368e42c57f9
fkaraagac11 <fehmi11.qa@gmail.com>
2020-08-01 23:28:21 -0400

    update

---
+++ CC/MyApp.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./components/Footer";
+import Footer from "./components/Header";
+import MainContent from "./components/MainContent";
+
+function MyApp() {
+    return (
+        <div>
+            <Header />
+            <MainContent />
+            <Footer />
+        </div>
+    );
+}
+
+export default MyApp;
---
+++ CC/components/MainContent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MainContent() {
+    return <div> This is MainContent</div>;
+}
+
+export default MainContent;
